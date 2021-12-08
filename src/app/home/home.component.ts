c1da4ac37546455bceed6f14c3725fde1ec56b2a
nguyenbalong123 <95390125+nguyenbalong123@users.noreply.github.com>
2021-12-08 15:14:39 +0700

    commm

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,14 +8,61 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name ='SANG BÁ';
   public age = 15;
+  public traiCay = ['Táo', 'Nho', 'Cam', 'Quyt'];
+  public traiCay2 = [
+    { ten: 'Táo', gia: 12, haGia: false},
+    { ten: 'Nho', gia: -3.2, haGia: true},
+    { ten: 'Cam', gia: 32.2, haGia: true},
+    { ten: 'Quyt', gia: 32.2, haGia: true},
+
+
+
+  ];
+  public vietnamData = [
+    {
+      city: 'An Giang' ,
+      district: [
+       "Thành phố Long Xuyên",
+       "Thành phố Châu Đốc",
+       "Thị xã Tân Châu",
+       "Huyện An Phú",
+       "Huyện Châu Phú",
+       "Huyện Châu Thành",
+       "Huyện Chợ Mới",
+       "Huyện Phú Tân",
+       "Huyện Thoại Sơn",
+       "Huyện Tịnh Biên",
+       "Huyện Tri Tôn",
+  ],
+},
+{
+  city: 'An Giang' ,
+  district: [
+   "Thành phố Long Xuyên",
+   "Thành phố Châu Đốc",
+   "Thị xã Tân Châu",
+   "Huyện An Phú",
+   "Huyện Châu Phú",
+   "Huyện Châu Thành",
+   "Huyện Chợ Mới",
+   "Huyện Phú Tân",
+   "Huyện Thoại Sơn",
+   "Huyện Tịnh Biên",
+   "Huyện Tri Tôn",
+   ],
+  },
+];
+
+
+
 
 
   constructor() { }
 
-  ngOnInit(): void {}
+ public ngOnInit(): void {
+   console.log('vietnamData = ', this.vietnamData);
+ }
+ 
 
-  public resetName(): void {
-    console.log('resetName');  
-    this.name = '';
-  }
+  
 }
